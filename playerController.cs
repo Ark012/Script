136499b584090cfe7fc88567f3d24a79f67003dc
FBBengtsson <58597698+FBBengtsson@users.noreply.github.com>
2019-12-13 15:27:49 +0100

    Create playerController.cs

---
+++ playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody Player;
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpForce;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Player.AddForce(movement * speed);
+
+        if (Input.GetKey("Jump"))
+        {
+            Player.AddForce(Vector2.up * jumpForce);
+        }
+
+
+    }
+}
