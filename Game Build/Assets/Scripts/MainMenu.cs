262d4c4293c6ca95bb599054d4a495e8f6d03337
Ark012 <58597341+Ark012@users.noreply.github.com>
2020-05-11 14:06:52 +0200

    New

--- Game Build/Assets/Scripts/MainMenu.cs
+++ Game Build/Assets/Scripts/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void QuitGame()
